ba9ff101340af233e04aefb4b587d69f3984e33a
AntonioFernando0 <jufer376@gmail.com>
2024-04-25 19:39:01 +0100

    javascript

---
+++ Aula06/js.js
@@ -0,0 +1,4 @@
+var nome = window.prompt(' Qual é seu nome?')
+document.write(`Olá, ${nome} seu nome tem ${nome.length} letras `)
+document.write(`Seu nome em maiúscula é ${nome.toUpperCase()}`)
+document.write(`Seu nome em minúscula é ${nome.toLowerCase()}`)
