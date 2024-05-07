e6931ffb472eb77fd067335e473614215d5c2d9c
AntonioFernando0 <jufer376@gmail.com>
2024-05-07 17:31:05 +0100

    js

--- Aula11/ex08.js
+++ Aula11/ex08.js
@@ -1,4 +1,4 @@
-let vel = 5
+let vel = 64
 console.log(`A velocidade do seu carro é ${vel}Km/h`)
 if ( vel > 60){ // Condição simples!
     console.log(`Você ultrapassaou a velocidade permitida. MULTADO!`)
--- Aula11/ex09.js
+++ Aula11/ex09.js
@@ -1,8 +1,48 @@
- var pais = 'EUA'
+   let pais = 'EUA'
  console.log (`Vivendo em ${pais}`)
  if (pais == 'Brasil'){
     console.log('Você é Brasileiro!')
  }
  else {
     console.log ('Você é Estrageiro!') 
- }
+ }       
+
+
+
+   let idade = 15
+         console.log (`Sua idade é ${idade}`)
+         if (idade >= 18) {
+           console.log ('Você é maior de idade') 
+         }
+         else {
+            console.log ('Você é menor de idade')
+         }
+
+  let numero = 10; 
+         
+         if (numero % 2 === 0) {
+             resultado = "par";
+         } else {
+             resultado = "ímpar";
+         }
+         
+         console.log(`O número é ${resultado}`);
+
+   
+
+let idade = 17;
+
+         if (idade >= 18) {
+             console.log("Você pode dirigir.");
+         } else {
+             console.log("Você não pode dirigir, espere até ter 18 anos.");
+         }
+
+ 
+
+
+
+
+
+
+    
