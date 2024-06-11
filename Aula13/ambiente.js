fadd3c3cf44cbb74783e5bd8a0cd17dcb0459cfb
AntonioFernando0 <jufer376@gmail.com>
2024-06-11 18:28:45 +0100

    Repetições
    Aprendendo sobre while e do while

---
+++ Aula13/ambiente.js
@@ -0,0 +1,22 @@
+let c = 1
+ do {
+    console.log(`Passo ${c}`)
+     c++ // c = c + 1 
+ } while (c <= 6)
+
+ 
+
+
+ /* let c = 1
+ while (c <= 6) {
+    console.log(`Passo ${c}`)
+     c++ // c = c + 1 
+ } */ // Usando somente o While
+
+/* console.log('Tudo bem') A forma mais dificil de estrutura de repetições
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem') */
