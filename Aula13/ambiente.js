37ddeeb691dc4200aaf9f43957e109ca2919bd05
AntonioFernando0 <jufer376@gmail.com>
2024-06-13 21:11:18 +0100

    Reptetições
    Utilizando FOR para repetições

--- Aula13/ambiente.js
+++ Aula13/ambiente.js
@@ -7,11 +7,11 @@ let c = 1
  
 
 
- /* let c = 1
+ let  = 1
  while (c <= 6) {
     console.log(`Passo ${c}`)
      c++ // c = c + 1 
- } */ // Usando somente o While
+ }  // Usando somente o While
 
 /* console.log('Tudo bem') A forma mais dificil de estrutura de repetições
 console.log('Tudo bem')
