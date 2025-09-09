925ae173d7bf19814ce3c29af250867cd5b6a167
Antonio Fernando <jufer376@gmail.com>
2025-09-08 21:21:28 -0300

    .
    .

--- RELOGIODIGITAL/index.js
+++ RELOGIODIGITAL/index.js
@@ -3,10 +3,16 @@
 
  function updateClock() {
 
+
+
 const today=new Date();
 const h=today.getHours();
 const m=today.getMinutes();
 const s=today.getSeconds();
+    if(m < 10){
+    m = "0" + m;
+}
+
 
 const msg = document.getElementById("clock");
 msg.innerText= `${h }: ${m } : ${s}`;
